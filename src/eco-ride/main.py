14f97eeb730c79557db3e0ff11e0578969c85656
Madhan <mmadhanjm@gmail.com>
2025-12-25 19:56:02 +0530

    add :[Madhan] provided encapsualtion and security

--- src/eco-ride/main.py
+++ src/eco-ride/main.py
@@ -7,7 +7,18 @@ class EcoRideMain:
     def basic_fleet_setup(self):
         v1=Vehicle("v1","v1-E",50)
         return v1
+    
+    def check_security(self):
+        v1=Vehicle("v1","v1-E",50)
+        print(v1.battery_level)
+        print(v1.rental_price)
+        v1.rental_price=10
+        print(v1.rental_price)
+        print(v1.maintenance_status)
+        v1.maintenance_status="good"
+        print(v1.maintenance_status)
+    
         
 er=EcoRideMain()
-print(er.basic_fleet_setup().model)
+er.check_security()
         
--- src/eco-ride/vehicle.py
+++ src/eco-ride/vehicle.py
@@ -3,3 +3,31 @@ class Vehicle():
         self.vehicle_id=vehicle_id
         self.model=model
         self.battery_level=battery_level
+        self.__rental_price=None
+        self.__maintenance_status=None
+        
+    def get_rental_price(self):
+        return self.__rental_price
+    
+    def set_rental_price(self,price):
+        if price<0:
+            raise ValueError("Negative rental price not allowed")
+        self.__rental_price=price        
+    rental_price=property(get_rental_price,set_rental_price)
+    
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+    
+    def set_maintenance_status(self,status):
+         self.__maintenance_status=status      
+    maintenance_status=property(get_maintenance_status,set_maintenance_status)
+    
+    def get_battery_level(self):
+        return self.__battery_level
+    
+    def set_battery_level(self,percentage):
+        if not  0<=percentage<=100:
+            raise ValueError("Battery Level should in Between 0 to 100")
+        self.__battery_level=percentage
+        
+    battery_level = property(get_battery_level, set_battery_level)
