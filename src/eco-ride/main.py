45224b3290d97fec735b8c7cdac4cec22aa4e866
Madhan <mmadhanjm@gmail.com>
2025-12-25 19:32:29 +0530

    add :[Madhan] added basic fleet setup

--- src/eco-ride/main.py
+++ src/eco-ride/main.py
@@ -1,6 +1,13 @@
+from vehicle import Vehicle
+
 class EcoRideMain:
     def greet(self):
         print("Welcome to Eco-Ride Urban Mobility System")
         
-er = EcoRideMain()
-er.greet()
+    def basic_fleet_setup(self):
+        v1=Vehicle("v1","v1-E",50)
+        return v1
+        
+er=EcoRideMain()
+print(er.basic_fleet_setup().model)
+        
---
+++ src/eco-ride/vehicle.py
@@ -0,0 +1,5 @@
+class Vehicle():
+    def __init__(self,vehicle_id,model,battery_level):
+        self.vehicle_id=vehicle_id
+        self.model=model
+        self.battery_level=battery_level
